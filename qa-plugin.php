e6124b2c1be5603420e3f7500f9de377eeaad50c
yaimapp <gmonday730-mbp@yahoo.co.jp>
2016-08-03 10:33:33 +0900

    Add Plugin file

---
+++ qa-plugin.php
@@ -0,0 +1,28 @@
+<?php
+
+/*
+	Plugin Name: Vote Page
+	Plugin URI:
+	Plugin Description: Add page of questions and answers that the user has voted.
+	Plugin Version: 1.0
+	Plugin Date: 2016-08-03
+	Plugin Author: 38qa.net
+	Plugin Author URI: http://38qa.net/
+	Plugin License: GPLv2
+	Plugin Minimum Question2Answer Version: 1.7
+	Plugin Update Check URI:
+*/
+
+if (!defined('QA_VERSION')) { // don't allow this page to be requested directly from browser
+	header('Location: ../../');
+	exit;
+}
+
+// process
+// qa_register_plugin_module('process', 'qa-vote-page-process.php', 'qa_vote_page_process', 'Vote Page Process');
+// layer
+// qa_register_plugin_layer('qa-vote-page-layer.php','Vote Page Layer');
+
+/*
+	Omit PHP closing tag to help avoid accidental output
+*/
